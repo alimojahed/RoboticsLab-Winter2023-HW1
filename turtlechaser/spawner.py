3ae80ce17d00acc7279e9f117f9641d8ec2e7b0b
alimojahed <alimojahed0079@gmail.com>
2023-03-03 23:44:37 -0500

    [FEAT]- spawn more turtles on a polygon

--- turtlechaser/spawner.py
+++ turtlechaser/spawner.py
@@ -9,30 +9,35 @@ from rclpy.node import Node
 
 from turtlesim.srv import Spawn, Kill
 from turtlesim.msg import Pose
-
+from .util import *
 class Spawner(Node):
     
     def __init__(self):
         super().__init__("spawner")
-        self.x_ = 0
-        self.y_ = 0
-        self.name_ = "spawned"
+
+        self.turtle_num = 6
+        self.name_prefix = "spawned_"
+        self.turtle_map = dict()
         self.spawn_turtle()
+        
         
         self.chaser_position_subscriber_ = self.create_subscription(Pose, "turtle1/pose", self.callback_chased, 10)
         
     def callback_chased(self, chaser_position):
-        if abs(chaser_position.x - self.x_) < 0.5 and abs(chaser_position.y - self.y_) < 0.5:
-            self.kill_turtle()
-            time.sleep(0.1)
-            self.spawn_turtle()
-        else: pass
+        for name, pos in self.turtle_map.items():
+            if abs(chaser_position.x - pos[0]) < 0.5 and abs(chaser_position.y - pos[1]) < 0.5:
+                self.kill_turtle(name)
+                time.sleep(0.1)
+                
+                #todo: publish name of turtle and order of killing
+                
+        
     
-    def kill_turtle(self):
+    def kill_turtle(self, name):
         turtle_killer = self.create_client(Kill, "kill")
         
         turtle_to_kill = Kill.Request()
-        turtle_to_kill.name = self.name_
+        turtle_to_kill.name = name
         
         future = turtle_killer.call_async(turtle_to_kill)
         future.add_done_callback(self.callback_killed_turtle)
@@ -44,21 +49,27 @@ class Spawner(Node):
             self.get_logger().error(e)
     
     def spawn_turtle(self):
-        spawner = self.create_client(Spawn, "spawn")
-        while not spawner.wait_for_service(1.0):
-            self.get_logger().warn("Waiting for the chaser to spawn..")
+        vertices = generate_polygon(center=(6, 6),
+                            avg_radius=3,
+                            irregularity=0,
+                            spikiness=0,
+                            num_vertices=6)
+
+        for i in range(len(vertices)):
             
-        turtle_to_spawn = Spawn.Request()
-        turtle_to_spawn.x = random.uniform(0.0, 11.0)
-        turtle_to_spawn.y = random.uniform(0.0, 11.0)
-        turtle_to_spawn.theta = random.uniform(-3, 3)
-        turtle_to_spawn.name = self.name_
-        
-        self.x_ = turtle_to_spawn.x
-        self.y_ = turtle_to_spawn.y
-        
-        future = spawner.call_async(turtle_to_spawn)
-        future.add_done_callback(partial(self.future_callback, spawned_x=turtle_to_spawn.x, spawned_y=turtle_to_spawn.y))
+            spawner = self.create_client(Spawn, "spawn")
+            while not spawner.wait_for_service(1.0):
+                self.get_logger().warn("Waiting for the chaser to spawn..")
+                
+            turtle_to_spawn = Spawn.Request()
+            turtle_to_spawn.x = vertices[i][0]
+            turtle_to_spawn.y = vertices[i][1]
+            turtle_to_spawn.theta = random.uniform(-3, 3)
+            turtle_to_spawn.name = self.name_prefix + str(i)
+            
+            
+            future = spawner.call_async(turtle_to_spawn)
+            future.add_done_callback(partial(self.future_callback, spawned_x=turtle_to_spawn.x, spawned_y=turtle_to_spawn.y))
         
     def future_callback(self, future, spawned_x, spawned_y):
         try:
